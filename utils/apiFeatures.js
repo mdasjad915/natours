3273332e5d35eb755e8a590dbdba924c9ace5ae0
Md Asjad Shakil <mdasjadshakil085@gmail.com>
2021-12-22 13:01:30 +0530

    Added refrence docs links as comments

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -6,10 +6,16 @@ class APIFeatures {
 
   filter() {
     const queryObj = { ...this.queryString };
+  
+    //excluded this field as we want to implement it seperately and it doesn't come under query
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     let queryStr = JSON.stringify(queryObj);
+    //After specifying any gte or lt like suymbol in url, 
+    // req.query = { difficulty : 'easy', duration : { gte: 5 }}
+    //IN MONGOOSE, We write as to filter: { difficulty : 'easy', duration : { $gte: 5 }}
+    //So just replacing that sign here...
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     this.query = this.query.find(JSON.parse(queryStr));
@@ -18,6 +24,7 @@ class APIFeatures {
 
   sort() {
     if (this.queryString.sort) {
+      // refer: https://mongoosejs.com/docs/api/query.html#query_Query-sort
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
@@ -30,6 +37,9 @@ class APIFeatures {
   limitFields() {
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(',').join(' ');
+
+      //it selects only the specifies in the "fields" variable defined above:
+      //for more detail: https://mongoosejs.com/docs/api/query.html#query_Query-select
       this.query = this.query.select(fields);
     } else {
       this.query = this.query.select('-__v');
@@ -38,6 +48,8 @@ class APIFeatures {
   }
 
   paginate() {
+    // limit defines the number of results per page
+    // page: the page that we want
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
@@ -45,6 +57,9 @@ class APIFeatures {
     this.query = this.query.skip(skip).limit(limit);
 
     return this;
+    // to get all documents that exists in a certain collecton, we van use "countDocuments"
+    //More: https://mongoosejs.com/docs/api/model.html#model_Model.countDocuments
+    //just to get total documents to help implement pagination
   }
 }
 
